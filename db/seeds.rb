6f0e2608d865dee59fbaf824ca810f2fdc131c48
Matt <matthewmorgante@gmail.com>
2015-03-10 13:01:15 +0800

    treatments and humanAPI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Treatment.create(name: "ACE Inhibitor", category_id: 1)
+Treatment.create(name: "Channel Blocker", category_id: 1)
+Treatment.create(name: "Diuretic", category_id: 1)
+Treatment.create(name: "Lisnopril", category_id: 1)
+Treatment.create(name: "Meditation", category_id: 1)
+Treatment.create(name: "Journaling", category_id: 1)
+Treatment.create(name: "30 minutes physical activity", category_id: 1)
+Treatment.create(name: "Low sodium diet", category_id: 1)
+Treatment.create(name: "Smoking Cessation", category_id: 1)
+
+Treatment.create(name: "Metformin", category_id: 4)
+Treatment.create(name: "Insulin Glargine", category_id: 4)
+Treatment.create(name: "30 minutes physical activity", category_id: 4)
+Treatment.create(name: "Vegeterian Diet", category_id: 4)
+Treatment.create(name: "Low Carb Diet", category_id: 4)
+Treatment.create(name: "High Fiber Diet", category_id: 4)
+
+
+Treatment.create(name: "Meditation", category_id: 3)
+Treatment.create(name: "30 minutes physical activity", category_id: 3)
+Treatment.create(name: "Eliminate caffeine", category_id: 3)
+Treatment.create(name: "Melatonin", category_id: 3)
+Treatment.create(name: "No electronics 1 hour before bed", category_id: 3)
+Treatment.create(name: "Evening routine", category_id: 3)
+Treatment.create(name: "Tylenol PM", category_id: 3)
+Treatment.create(name: "Ambien", category_id: 3)
+
+
+Treatment.create(name: "Low Carb Diet", category_id: 2)
+Treatment.create(name: "High Fat Diet", category_id: 2)
+Treatment.create(name: "Weight Training", category_id: 2)
+Treatment.create(name: "High Intensity Interval Training (HIIT)", category_id: 2)
+Treatment.create(name: "10,000 steps", category_id: 2)
+Treatment.create(name: "Caloric Restriction", category_id: 2)
+Treatment.create(name: "30 minutes physical activity", category_id: 2)
+Treatment.create(name: "Running", category_id: 2)
+
+Treatment.create(name: "Statins", category_id: 5)
+Treatment.create(name: "Lipitor", category_id: 5)
+Treatment.create(name: "Crestor", category_id: 5)
+Treatment.create(name: "Low Cholesterol Diet", category_id: 5)
+Treatment.create(name: "30 minutes physical activity", category_id: 5)
+Treatment.create(name: "High Fiber Diet", category_id: 5)
+Treatment.create(name: "Fish Oil Supplement", category_id: 5)
+
