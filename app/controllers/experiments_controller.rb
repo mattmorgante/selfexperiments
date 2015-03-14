f6735b64c0841b14b5ce26f8c3bc4ad80dedea0a
Matt <matthewmorgante@gmail.com>
2015-03-14 14:42:58 +0800

    hidden fields added

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -16,6 +16,7 @@ class ExperimentsController < ApplicationController
   def new
     @experiment = Experiment.new
     @category = Category.find(params[:category_id])
+    @user = User.new
   end
 
   # GET /experiments/1/edit
